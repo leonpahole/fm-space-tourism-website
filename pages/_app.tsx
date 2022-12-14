a7e283bf6b687618fefb67369e4e5a0886af2e10
Leon Pahole <leon.pahole@gmail.com>
2022-12-14 23:04:34 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/index.scss';
+import type { AppProps } from 'next/app';
+import { barlowCondensedFont, barlowFont, bellefairFont } from '../utils/font-family.utils';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style jsx global>
+        {`
+          html {
+            --bellefair-font: ${bellefairFont.style.fontFamily};
+            --barlow-condensed-font: ${barlowCondensedFont.style.fontFamily};
+            --barlow-font: ${barlowFont.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ utils/font-family.utils.ts
@@ -0,0 +1,13 @@
+import { Bellefair, Barlow_Condensed, Barlow } from '@next/font/google';
+
+export const bellefairFont = Bellefair({ weight: ['400'], subsets: ['latin'] });
+
+export const barlowCondensedFont = Barlow_Condensed({
+  weight: ['400'],
+  subsets: ['latin'],
+});
+
+export const barlowFont = Barlow({
+  weight: ['400'],
+  subsets: ['latin'],
+});
